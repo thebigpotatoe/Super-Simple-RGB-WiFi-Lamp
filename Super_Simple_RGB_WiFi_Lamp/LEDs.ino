1cac87cb355dbf363868e09ce7360a5c47c41360
Jennifer Lang <518343+jennytoo@users.noreply.github.com>
2019-12-15 23:08:29 -0800

    : Set currentMode on start, fix led flicker
    * LED update is based on the currentMode variable, not the Mode
    variable. On initial startup getConfig sets Mode, but currentMode is
    left unset. As this isn't a valid mode the refresh loop sets both values
    to the default Colour, requiring the config be rerear. FIX: Set
    currentMode when initializing the LEDs.
    * More complex modes make the LEDs flicker; instead of running the
    render code every update add a toggle to alternate between render or
    show giving other code a chance to run between slow renders and
    strip refresh. Most likely being caused by wifi processing/buffering and
    this fixes it on my devices.

--- Super_Simple_RGB_WiFi_Lamp/LEDs.ino
+++ Super_Simple_RGB_WiFi_Lamp/LEDs.ino
@@ -1,55 +1,76 @@
-void ledStringInit() {
+void ledStringInit()
+{
   // add the leds to fast led and clear them
   FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(ledString, NUM_LEDS);
-  FastLED.clear ();
+  FastLED.clear();
   FastLED.show();
 
   // Set the maximum power draw
   // FastLED.setMaxPowerInVoltsAndMilliamps(5,1000);
 
+  // Set initial mode
+  currentMode = "Colour"; // Automatically jump back to colour
+
   // Debug
   Serial.println("[handleMode] - LED string was set up correctly");
 }
 
-void handleMode() {
+bool renderPass = true;
+void handleMode()
+{
   // Adapt the leds to the current mode
 
   auto modeIter = modes.find(currentMode);
-  if (modeIter == modes.end()) {
+  if (modeIter == modes.end())
+  {
     // not found
     Serial.println("[handleMode] - Mode \"" + Mode + "\" not found, resetting to default");
-    Mode = "Colour";    // Automatically jump back to colour
-    currentMode = "Colour";    // Automatically jump back to colour
+    Mode = "Colour";        // Automatically jump back to colour
+    currentMode = "Colour"; // Automatically jump back to colour
     return;
   }
-  else {
+  else
+  {
     // Apply currentMode always?
 
-    // If mode is found run its render function
-    modeIter->second->render();
+    if (renderPass)
+    {
+      // If mode is found run its render function
+      modeIter->second->render();
 
-    // Globally adjust the brightness
-    adjustBrightness();
-
-    // Handle Fast LED
-    FastLED.show();
-    //  FastLED.delay(1000 / FRAMES_PER_SECOND);
-  }  
+      // Globally adjust the brightness
+      adjustBrightness();
+    }
+    else
+    {
+      // Handle Fast LED
+      FastLED.show();
+      //  FastLED.delay(1000 / FRAMES_PER_SECOND);
+    }
+    // Let other code run before we do the update
+    renderPass = !renderPass;
+  }
 }
 
-void adjustBrightness() {
+void adjustBrightness()
+{
   // Adjust the brightness depending on the mode
-  if (autoOnWithModeChange || State) {
-    if (Mode != currentMode) {
-      // Dim lights off first 
-      if (modeChangeFadeAmount > 0) {
+  if (autoOnWithModeChange || State)
+  {
+    if (Mode != currentMode)
+    {
+      // Dim lights off first
+      if (modeChangeFadeAmount > 0)
+      {
         // Set the dimming variables and apply
-        EVERY_N_MILLISECONDS(20) {
-          modeChangeFadeAmount -= (FadeTime > 0) ? (255 / ((float)FadeTime/20)) : 255;
+        EVERY_N_MILLISECONDS(20)
+        {
+          modeChangeFadeAmount -= (FadeTime > 0) ? (255 / ((float)FadeTime / 20)) : 255;
           modeChangeFadeAmount = constrain(modeChangeFadeAmount, 0, 255);
         };
       }
-      else {
+      else
+      {
         // Debug
         Serial.println("[handleMode] - Mode changed to: " + Mode);
 
@@ -61,15 +82,19 @@ void adjustBrightness() {
         modeChangeFadeAmount = 0;
       }
     }
-    else if (currentMode != previousMode) {
+    else if (currentMode != previousMode)
+    {
       // On mode change dim lights up
-      if (modeChangeFadeAmount < 255) {
-        EVERY_N_MILLISECONDS(20) {
-          modeChangeFadeAmount += (FadeTime > 0) ? (255 / ((float)FadeTime/20)) : 255;
+      if (modeChangeFadeAmount < 255)
+      {
+        EVERY_N_MILLISECONDS(20)
+        {
+          modeChangeFadeAmount += (FadeTime > 0) ? (255 / ((float)FadeTime / 20)) : 255;
           modeChangeFadeAmount = constrain(modeChangeFadeAmount, 0, 255);
         };
       }
-      else {
+      else
+      {
         // Set the currentMode to Mode
         previousMode = currentMode;
       }
@@ -77,15 +102,19 @@ void adjustBrightness() {
   }
 
   // Adjust the brightness depending on the state
-  if (!State && previousState) {
+  if (!State && previousState)
+  {
     // Turn Lights off slowly
-    if (modeChangeFadeAmount > 0) {
-      EVERY_N_MILLISECONDS(20) {
-        modeChangeFadeAmount -= (FadeTime > 0) ? (255 / ((float)FadeTime/20)) : 255;
+    if (modeChangeFadeAmount > 0)
+    {
+      EVERY_N_MILLISECONDS(20)
+      {
+        modeChangeFadeAmount -= (FadeTime > 0) ? (255 / ((float)FadeTime / 20)) : 255;
         modeChangeFadeAmount = constrain(modeChangeFadeAmount, 0, 255);
       };
     }
-    else {
+    else
+    {
       // Debug
       Serial.println("[handleMode] - LED's turned off");
 
@@ -93,16 +122,20 @@ void adjustBrightness() {
       previousState = false;
     }
   }
-  else if (State && !previousState) {
+  else if (State && !previousState)
+  {
     // Turn on light slowly
-    if (modeChangeFadeAmount < 255) {
-      EVERY_N_MILLISECONDS(20) {
-        modeChangeFadeAmount += (FadeTime > 0) ? (255 / ((float)FadeTime/20)) : 255;
+    if (modeChangeFadeAmount < 255)
+    {
+      EVERY_N_MILLISECONDS(20)
+      {
+        modeChangeFadeAmount += (FadeTime > 0) ? (255 / ((float)FadeTime / 20)) : 255;
         modeChangeFadeAmount = constrain(modeChangeFadeAmount, 0, 255);
       };
     }
-    else {
-      // Debug 
+    else
+    {
+      // Debug
       Serial.println("[handleMode] - LED's turned on");
 
       // Set the previous values
